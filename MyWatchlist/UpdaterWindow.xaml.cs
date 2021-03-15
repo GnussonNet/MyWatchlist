970e3eff115b716d40a17b3233ca9be7bbf676b3
Gnusson.net <spel@gnusson.net>
2021-03-15 17:32:23 +0100

    Nothing

---
+++ MyWatchlist/UpdaterWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyWatchlist
+{
+    /// <summary>
+    /// Interaction logic for UpdaterWindow.xaml
+    /// </summary>
+    public partial class UpdaterWindow : Window
+    {
+        public UpdaterWindow()
+        {
+            InitializeComponent();
+        }
+        #region --- ChromeWindow ---
+        // Can execute
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        // Minimize
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        // Close
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        // State change
+        private void MainWindowStateChangeRaised(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                MainWindowBorder.BorderThickness = new Thickness(8);
+            }
+            else
+            {
+                MainWindowBorder.BorderThickness = new Thickness(0);
+            }
+        }
+        #endregion
+    }
+}
